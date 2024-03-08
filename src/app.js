a514ffa65d1486b3a30dca68c434e2e9f20a329b
My'lik Kerley <159506912+KerleyCode@users.noreply.github.com>
2024-03-08 18:19:06 +0000

    finished Excuse Project

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let element = document.querySelector(".excuse");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  const getRandomInt = array => {
+    return Math.floor(Math.random() * array.length);
+  };
+  let excuse =
+    who[getRandomInt(who)] +
+    " " +
+    action[getRandomInt(action)] +
+    " " +
+    what[getRandomInt(what)] +
+    " " +
+    when[getRandomInt(when)] +
+    ".";
+  element.innerHTML = excuse;
 };
